9179302dea5847913cbce7f6b20bf5f48278e2a7
Benjamin Nneji <nnejiben@gmail.com>
2025-03-08 17:51:42 +0100

    Update SettingsAddUser.jsx

--- src/pages/dashboard/SettingsAddUser.jsx
+++ src/pages/dashboard/SettingsAddUser.jsx
@@ -12,7 +12,41 @@ export default function SettingsAddUser({ setView }) {
                 Back
             </button>
 
-            <div>Add User content</div>
+            {/* New user */}
+            <div>
+                {/* Title */}
+                <div className="mb-7.5">
+                    <h1 className="text-2xl text-dark-primary font-semibold mb-1">
+                        New User
+                    </h1>
+                    <p className="text-xs text-gray-757575 font-medium">
+                        Add a new user
+                    </p>
+                </div>
+                {/* Input user email */}
+                <div className="mb-16">
+                    <input 
+                        type="text" 
+                        name="" 
+                        className="bg-white border border-black-10-percent text-blue-001b2a text-base rounded-[10px] focus:outline-none focus:ring-4 focus:ring-blue-300 block w-full p-4 placeholder:text-[#E5E5E5]" 
+                        placeholder="Input User’s Email" 
+                        required 
+                    />
+                </div>
+            </div>
+
+            {/* Choose role */}
+            <div>
+                {/* Title */}
+                <div className="mb-7.5">
+                    <h1 className="text-2xl text-dark-primary font-semibold mb-1">
+                        Choose Role
+                    </h1>
+                    <p className="text-xs text-gray-757575 font-medium">
+                        Assign new user to a role
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
