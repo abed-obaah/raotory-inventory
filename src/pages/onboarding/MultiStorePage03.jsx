4f62aebbadb4b925e72c23848da449b87691f00d
Benjamin Nneji <nnejiben@gmail.com>
2025-03-17 19:53:41 +0100

    Update MultiStorePage03.jsx

--- src/pages/onboarding/MultiStorePage03.jsx
+++ src/pages/onboarding/MultiStorePage03.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import MultiStorePage02 from "./MultiStorePage02";
+import StoreSetupSuccess from "/src/assets/store-setup-success.png";
 // import MultiStorePage04 from "./MultiStorePage04";
 
 export default function MultiStorePage03() {
@@ -20,8 +21,43 @@ export default function MultiStorePage03() {
                         Back
                     </button>
 
-                    {/* Main content here */}
-                    <h2>Multi Store Page 03 main content</h2>
+                    {/* Multi store set up congratulations */}
+                    <div className="flex flex-col w-full max-w-[536px] mx-auto mt-10">
+                        
+                        {/* Heading */}
+                        <div className="mb-7 text-center">
+                            <h2 className="text-2xl font-semibold mb-2">
+                                Multi store
+                            </h2>
+                            <p className="text-gray-600 text-base max-w-[468px] mx-auto">
+                                This gives you the ability to set up your store and inventory appropriately
+                            </p>
+                        </div>
+            
+                        {/* Form */}
+                        <div className="w-full border border-black-10-percent rounded-3xl p-10.5">
+                            <form action="#" method="POST" className="">
+
+                                {/* Image */}
+                                <div className="">
+                                    <img src={StoreSetupSuccess} alt="Succes image" className="w-full h-auto object-cover" />
+                                </div>
+                                <h4 className="text-4xl font-bold text-center mb-4.5">Yay!</h4>
+                                <p className="text-center">Congratulations, you have successfully set up your store(s) on Raotory, Your stock keeping buddy!</p>
+
+                                {/* Button */}
+                                <div className="mb-4 mt-10">
+                                    <button
+                                        disabled
+                                        onClick={() => setView("multi-store-page-04")}
+                                        className="flex w-full justify-center bg-blue-primary hover:bg-blue-0e90da focus:outline-none focus:ring-4 focus:ring-blue-300 font-semibold rounded-[10px] text-base p-4 text-center me-2"
+                                    >
+                                        Continue
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
 
                     {/* Next button */}
                     <div className="w-full max-w-[340px] mx-auto mt-10 hidden">
