bccabba57bdcf98fac0fb22a4f9f4fd63a9b42a3
Benjamin Nneji <nnejiben@gmail.com>
2025-03-24 21:17:27 +0100

    Update InvoiceViewInvoice.jsx

--- src/pages/dashboard/InvoiceViewInvoice.jsx
+++ src/pages/dashboard/InvoiceViewInvoice.jsx
@@ -68,6 +68,7 @@ export default function InvoiceViewInvoice({ onBack }) {
                     <thead className="text-sm text-gray-757575 bg-gray-e5e5e5">
                         <tr>
                             <th className="px-2.5 py-3 border border-black-10-percent">S/N</th>
+                            <th className="px-2.5 py-3 border border-black-10-percent">Product Name</th>
                             <th className="px-2.5 py-3 border border-black-10-percent">Cost Price</th>
                             <th className="px-2.5 py-3 border border-black-10-percent">Selling Price</th>
                             <th className="px-2.5 py-3 border border-black-10-percent">Quantity</th>
@@ -76,52 +77,79 @@ export default function InvoiceViewInvoice({ onBack }) {
                     </thead>
                     <tbody>
                         <tr className="bg-white hover:bg-gray-100">
+                            {/* Serial number */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 01
                             </td>
+                            {/* Product name */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 Paracetamol
                             </td>
+                            {/* Cost price */}
+                            <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
+                                NGN<span>10,000</span>
+                            </td>
+                            {/* Selling price */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 NGN<span>15,000</span>
                             </td>
+                            {/* Quantity */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 120
                             </td>
+                            {/* Total */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 NGN<span>250,000</span>
                             </td>
                         </tr>
                         <tr className="bg-white hover:bg-gray-100">
+                            {/* Serial number */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 01
                             </td>
+                            {/* Product name */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 Paracetamol
                             </td>
+                            {/* Cost price */}
+                            <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
+                                NGN<span>10,000</span>
+                            </td>
+                            {/* Selling price */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 NGN<span>15,000</span>
                             </td>
+                            {/* Quantity */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 120
                             </td>
+                            {/* Total */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 NGN<span>250,000</span>
                             </td>
                         </tr>
                         <tr className="bg-white hover:bg-gray-100">
+                            {/* Serial number */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 01
                             </td>
+                            {/* Product name */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 Paracetamol
                             </td>
+                            {/* Cost price */}
+                            <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
+                                NGN<span>10,000</span>
+                            </td>
+                            {/* Selling price */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 NGN<span>15,000</span>
                             </td>
+                            {/* Quantity */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 120
                             </td>
+                            {/* Total */}
                             <td className="px-2.5 py-2 border border-black-10-percent text-gray-757575">
                                 NGN<span>250,000</span>
                             </td>
