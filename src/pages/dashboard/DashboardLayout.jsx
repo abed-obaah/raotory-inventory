d091f08624fcd5e580391c627fe39e0a9af16762
Benjamin Nneji <nnejiben@gmail.com>
2025-03-27 21:00:12 +0100

    Update DashboardLayout.jsx

--- src/pages/dashboard/DashboardLayout.jsx
+++ src/pages/dashboard/DashboardLayout.jsx
@@ -119,7 +119,6 @@ const handleStoreSelection = (store) => {
     setStoreName(store.name);
 };
 
-
   const components = {
       "Overview": () => <Overview setSelectedComponent={setSelectedComponent} />,  // ✅ Pass the prop
       "Input Order": () => <InputOrder setSelectedComponent={setSelectedComponent} />, // ✅ Pass the prop
@@ -657,41 +656,41 @@ const handleStoreSelection = (store) => {
 
                             {/* Profile dropdown */}
                             <Menu as="div" className="relative">
-            <MenuButton className="-m-1.5 flex items-center p-1.5">
-                <span className="sr-only">Open user menu</span>
-                <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    className="size-8 rounded-full bg-gray-50"
-                />
-                <span className="hidden lg:flex lg:items-center">
-                    <span
-                        aria-hidden="true"
-                        className="ml-4 text-sm font-semibold text-gray-900"
-                    >
-                        {storeName}
-                    </span>
-                    <ChevronDownIcon
-                        aria-hidden="true"
-                        className="ml-2 size-5 text-gray-400"
-                    />
-                </span>
-            </MenuButton>
-            <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2.5 w-40 origin-top-right rounded-md bg-white py-2 ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-            >
-                {userNavigation.map((item, index) => (
-                    <MenuItem key={index} onClick={() => handleStoreSelection(item)}>
-                        <button
-                            className="w-full text-left px-3 py-1 text-sm text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
-                        >
-                            {item.name}
-                        </button>
-                    </MenuItem>
-                ))}
-            </MenuItems>
-        </Menu>
+                                <MenuButton className="-m-1.5 flex items-center p-1.5">
+                                    <span className="sr-only">Open user menu</span>
+                                    <img
+                                        alt=""
+                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        className="size-8 rounded-full bg-gray-50"
+                                    />
+                                    <span className="hidden lg:flex lg:items-center">
+                                        <span
+                                            aria-hidden="true"
+                                            className="ml-4 text-sm font-semibold text-gray-900"
+                                        >
+                                            {storeName}
+                                        </span>
+                                        <ChevronDownIcon
+                                            aria-hidden="true"
+                                            className="ml-2 size-5 text-gray-400"
+                                        />
+                                    </span>
+                                </MenuButton>
+                                <MenuItems
+                                    transition
+                                    className="absolute right-0 z-10 mt-2.5 w-40 origin-top-right rounded-md bg-white py-2 ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                                >
+                                    {userNavigation.map((item, index) => (
+                                        <MenuItem key={index} onClick={() => handleStoreSelection(item)}>
+                                            <button
+                                                className="w-full text-left px-3 py-1 text-sm text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
+                                            >
+                                                {item.name}
+                                            </button>
+                                        </MenuItem>
+                                    ))}
+                                </MenuItems>
+                            </Menu>
                         </div>
                     </div>
                 </div>
